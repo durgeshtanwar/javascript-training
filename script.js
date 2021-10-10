8b9ed932a0a436b3ffcf9fee1ca692e6a7a0caba
unknown <tanwardurgesh@gmail.com>
2021-10-10 21:58:30 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,122 @@
+// day 1
+
+// console.log('Durgesh Tanwar');
+// let js="amazing";
+// if(js==="amazin"){
+//     alert('its amazing');
+// }
+// else{
+//     alert("its bori
+
+// day 2
+// console.log(9);
+// console.log('Arvind');
+// console.log("Manish");
+// console.log(32.9);
+
+
+// lets talk about variables 
+// let firstName = "Prem";
+// let lastName = "Sankhla";
+// let address = "B-50 A Murlidhar vyas Nagar Karmisar road Bikaner."
+
+
+// console.log('Shyamlal');
+// console.log(22);
+// console.log("Mohta Chowk");
+// console.log(45+99);
+
+
+// let firstName = "Durgesh";
+// let lastName = "Kapoor";
+
+// console.log(firstName);
+
+// let address = "B-50 A Murlidhar vyas Nagar Karmisar road bikaner";
+////////////////////////////////////////////////////
+
+// Data types.
+// there are 2 types of data types that
+//  1 primitive and other is objects.
+//  objects are written in the curly brackets.
+// example
+// let a = {
+//     name:"John",
+//     age:25,
+//     city: "London",
+// };
+
+// any data type which is not objects are primitive type of data types.
+// there are 7 types of primitive datatypes.
+// 1 Number 
+// 2 String
+// 3 boolean
+// 4 undefined
+// 5 null
+// 6 bigInt (Larger integers than the Number type can hold)
+// 7 Symbol (not very important or useful for now. it was introduced in ES 2020)
+
+//  Dynamic typing: Javascript support dynamic typing which means we do manually define the datatypes data types are determined automatically
+// 1 Numbers are always decimal types.
+    // example let a = 23; it is equal to 23.0
+    
+// 2 String is a sequence of charecters.
+// 3. boolean is true or false
+
+// there is a difference between value and variable that a variables do not have data types the values have 
+
+//  Dynamic typing example let a =
+// let fname = "Mark";
+// console.log(fname);
+
+// fname = 45.55;
+// console.log(fname);
+
+// console.log(typeof(34));
+// console.log(typeof(true));
+// var declaredButNotUsed;
+// console.log(typeof(declaredButNotUsed));
+// console.log(typeof(null));
+// console.log(typeof('Jai Mata Di'));
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// javascriptIsFUn = 'Yes';
+
+// String 
+// string is the sequence of the charecters or numbers but it should be inside double quote or single quote " " ' ';
+// let a = "school";
+// let b = 22;
+// Booleam
+
+let isTrueOrFalse = true;
+let falseOrTrue = false;
+
+let a = 20;
+let b = null;
+
+
+// console.log(b);
+
+//  console.log(typeof('gaurav'));
+console.log(typeof(b));
+// console.log(typeof(true));
+
+// let input = "abhishek";
+
+// if(typeof(input)=='string'){
+//     console.log('string');
+// }
+
+
+let name = "Shyamlal";
+console.log(typeof(name));
+
+name = 22;
+console.log(typeof(name));
+
+let js ="Javascript is fun";
+console.log(js);
+js = 22510;
+console.log(js);
+
---
+++ upcoming.js
@@ -0,0 +1,38 @@
+// Data types.
+// there are 2 types of data types that
+//  1 primitive and other is objects.
+//  objects are written in the curly brackets.
+// example
+// let a = {
+//     name:"John",
+//     age:25,
+//     city: "London",
+// };
+
+// any data type which is not objects are primitive type of data types.
+// there are 7 types of primitive datatypes.
+// 1 Number 
+// 2 String
+// 3 boolean
+// 4 undefined
+// 5 null
+// 6 bigInt (Larger integers than the Number type can hold)
+// 7 Symbol (not very important or useful for now. it was introduced in ES 2020)
+
+//  Dynamic typing: Javascript support dynamic typing which means we do manually define the datatypes data types are determined automatically
+// 1 Numbers are always decimal types.
+    // example let a = 23; it is equal to 23.0
+    
+// 2 String is a sequence of charecters.
+// 3. boolean is true or false
+
+// there is a difference between value and variable that a variables do not have data types the values have 
+
+//  Dynamic typing example let a =
+// let fname = "Mark";
+// console.log(fname);
+
+// fname = 45.55;
+// console.log(fname);
+
+console.log(typeof(34));
