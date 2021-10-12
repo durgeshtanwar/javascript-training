cece067f741c731648520b42941b130879dbd427
unknown <tanwardurgesh@gmail.com>
2021-10-12 12:16:49 +0530

    we have done opertors class

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 // Operators
 //  arithmetic Operators
 //*  addition (+) operators
-let a = 5;
-let b = 10;
-console.log(a+b);
+//let a = 5;
+//let b = 10;
+//console.log(a+b);
 
 
 //* Subtraction (-) operators
 
-let firstNumber = 10;
-let secondNumber = 15;
+//let firstNumber = 10;
+//let secondNumber = 15;
 
 // console.log(secondNumber - firstNumber);
 //* Multiplication (*) operators
 
-// console.log('multiplication = '+a*b);
+//console.log('multiplication = '+a*b);
 // * division  
-// console.log('division = ' + b/a);
+//console.log('division = ' + b/a);
 // * Modulus Operator 
 // ? this modulus operator gets the result we get the remaining of the given division.
 // ? example of positive divident
-//  console.log (12 % 5);
+ // console.log ("modulus operator " + -12 % 5);
 // ? 1 % -2 = 1
 // ? 1 % 2  =  1
 // ? 2 % 3  =  2
@@ -34,23 +34,23 @@ let secondNumber = 15;
 
 //* Exponentiation Operator (**) 
 // ? this operator gets the power of the value for example when we say 2's power 2 is 4 or 2's power 3 is 8
-console.log('** operator 12 ki power 4 = '+12**4); 
+console.log('** operator 12 ki power 2 = '+12**2); 
 
 // * Increment (++) and decrement (--) operator
 // ? this operator adds one to the value
 //todo example of increment
 
-// let firstVal = 10;
+let firstVal = 10;
 // console.log(firstVal++);
-// console.log(firstVal);
+//  console.log(firstVal);
 
-// console.log(++firstVal);
+//  console.log(++firstVal);
 
 // todo example of the decrement operator
 
 // console.log(firstVal--);
 // console.log(firstVal);
-// console.log(--firstval);
+// console.log(--firstVal);
 
 //! String Operators
 //* Concatenation in javascript also happens with + operators
@@ -60,11 +60,11 @@ console.log('** operator 12 ki power 4 = '+12**4);
 // console.log(firstName + lastName);
 //  console.log(firstName + ' ' + lastName);
 
-// let x = 5 + 5;
+// let x1 = 5 + 5;
 // let y = "5" + 5;
 // let z = "Hello" + 5;
 
-// console.log(x,y,z);
+// console.log(x1,y,z);
 
 
 //* assignment Operators
@@ -73,30 +73,35 @@ console.log('** operator 12 ki power 4 = '+12**4);
 // todo first one is = (equal)
 
 // examples 
-let a = 7;
+ let a = 7;
 let b = a;
 
 // ? += 
 console.log(a += b) //? it is same as a = a + b;
-
+console.log('value of a = '+a);
+console.log('value of b = '+b);
 // -=
 console.log(a -= b); //? it is same as a = a - b;
-
+console.log('value of a = '+a);
+console.log('value of b = '+b)
 // todo *=
 console.log(a*=b); //? same as a = a * b;
-
+console.log('value of a = '+a);
+console.log('value of b = '+b)
 // todo /=
 console.log(a/=b); //? same as a = a / b;
-
+console.log('value of a = '+a);
+console.log('value of b = '+b)
 // todo %= //
 console.log(a%=b); //? same as a = a % b;
-
+console.log('value of a = '+a);
+console.log('value of b = '+b)
 // todo **=
 console.log(a**=b); //? same as a = a**b;
-
-let x = 10;
-x += 5;
-
+console.log('value of a = '+a);
+console.log('value of b = '+b)
+// let x = 10;
+// x += 5;
 // * JavaScript Comparison Operators
 
 // * ==	equal to
@@ -107,18 +112,21 @@ x += 5;
 //* <	less than
 //* >=	greater than or equal to
 //* <=	less than or equal to
-
-// ==	equal to	x == 8	false	
+let x=5;
+// ==	equal to	x == 8	false
+// console.log(5=="5");
+// console.log(5==="5");	
 // x == 5	true	
 // x == "5"	true	
 // ===	equal value and equal type	x === 5	true	
 // x === "5"	false	
-// !=	not equal	x != 8	true	
+// !=	not equal	x != 8	true !==
 // !==	not equal value or not equal type	x !== 5	false	
 // x !== "5"	true	
 // x !== 8	true	
 // >	greater than	x > 8	false	
 // <	less than	x < 8	true	
+// console.log(x>=6);
 // >=	greater than or equal to	x >= 8	false	
 // <=	less than or equal to	x <= 8	true
 
@@ -128,12 +136,47 @@ x += 5;
 //* || logical or
 //* ! logical not
 
+let a1 = 4;
+let b1 = 7;
+// And (&&) Logical operator
+if(a1>2 && b1>8 ){
+    console.log(true);
+}
+else{
+    console.log(false)
+}
+
+if(a1>2 && b1>6 ){
+    console.log(true);
+}
+else{
+    console.log(false)
+}
+
+// Or Operator
+if(a1<2 || b1>8 ){
+    console.log(true);
+}
+else{
+    console.log(false)
+}
+
+if(a1>2 || b1>6 ){
+    console.log(true);
+}
+else{
+    console.log(false)
+}
+
+
+
 // todo JavaScript Type Operators
 // typeof (Returns the type of a variable)
 // instanceof (Returns true if an object is an instance of an object type)
 
 // Conditional Operators (Ternary Operators)
-
-// variablename = (condition) ? value1:value2 
-// let voteable = (age > 18) ? 'he can vote' : 'He can not';
-
+let age= 26;
+//  variablename = (condition) ? value1:value2 
+let voteable = (age >= 18) ? 'he can vote' : 'He can not vote';
+console.log(voteable);
+// Conditional Operator 
