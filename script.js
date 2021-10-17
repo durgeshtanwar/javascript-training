88470eac3cda2ddcc54aca715c49e5413e46a521
unknown <tanwardurgesh@gmail.com>
2021-10-17 22:37:36 +0530

    Operator-precedence

--- script.js
+++ script.js
@@ -1,182 +1,59 @@
-// Operators
-//  arithmetic Operators
-//*  addition (+) operators
-//let a = 5;
-//let b = 10;
-//console.log(a+b);
 
+// Operator Precedence
+/* 
+the order in which operators works if there are more then one operators is called operator precedence. 
 
-//* Subtraction (-) operators
+Operator precedence determines how operators are parsed concerning each other.
+Operators with higher precedence become the operands of operators with lower precedence.
 
-//let firstNumber = 10;
-//let secondNumber = 15;
+*/
+let now = 2021;
+console.log(now - 1989 > now - 2000 );
+// why subtraction happened before the comparision?
+// This is made possible because of operator precedence
 
-// console.log(secondNumber - firstNumber);
-//* Multiplication (*) operators
+console.log(3+4*5);
+// ? What will be the output of this?
 
-//console.log('multiplication = '+a*b);
-// * division  
-//console.log('division = ' + b/a);
-// * Modulus Operator 
-// ? this modulus operator gets the result we get the remaining of the given division.
-// ? example of positive divident
- // console.log ("modulus operator " + -12 % 5);
-// ? 1 % -2 = 1
-// ? 1 % 2  =  1
-// ? 2 % 3  =  2
-// ? 5.5 % 2 = 1.5
+console.log(4 * 3 ** 2); // 4 * 9
 
+// ? what will be output of this?
 
-//! example of negetive divident
-//? -12 % 5 // -2 
-//? -1 % 2  // -1
-//? -4 % 2  // -0
+let a;
+let b;
 
-//* Exponentiation Operator (**) 
-// ? this operator gets the power of the value for example when we say 2's power 2 is 4 or 2's power 3 is 8
-console.log('** operator 12 ki power 2 = '+12**2); 
+console.log(a = b = 5);
+// expected output: 5
 
-// * Increment (++) and decrement (--) operator
-// ? this operator adds one to the value
-//todo example of increment
+// In below example if the operator op2 have higher precedence then op2 will be solved first.
+// a OP1 b OP2 c
 
-let firstVal = 10;
-// console.log(firstVal++);
-//  console.log(firstVal);
+console.log(3 + 10 * 2);   // logs 23
+console.log(3 + (10 * 2)); // logs 23 because parentheses here are superfluous
+console.log((3 + 10) * 2); // logs 26 because the parentheses change the order
 
-//  console.log(++firstVal);
-
-// todo example of the decrement operator
-
-// console.log(firstVal--);
-// console.log(firstVal);
-// console.log(--firstVal);
-
-//! String Operators
-//* Concatenation in javascript also happens with + operators
-// let firstName = "Nandini";
-// let lastName = "Sankhla";
-
-// console.log(firstName + lastName);
-//  console.log(firstName + ' ' + lastName);
-
-// let x1 = 5 + 5;
-// let y = "5" + 5;
-// let z = "Hello" + 5;
-
-// console.log(x1,y,z);
-
-
-//* assignment Operators
-// ? assignment operators assign value to variables
-// ? they are of different types
-// todo first one is = (equal)
-
-// examples 
- let a = 7;
-let b = a;
-
-// ? += 
-console.log(a += b) //? it is same as a = a + b;
-console.log('value of a = '+a);
-console.log('value of b = '+b);
-// -=
-console.log(a -= b); //? it is same as a = a - b;
-console.log('value of a = '+a);
-console.log('value of b = '+b)
-// todo *=
-console.log(a*=b); //? same as a = a * b;
-console.log('value of a = '+a);
-console.log('value of b = '+b)
-// todo /=
-console.log(a/=b); //? same as a = a / b;
-console.log('value of a = '+a);
-console.log('value of b = '+b)
-// todo %= //
-console.log(a%=b); //? same as a = a % b;
-console.log('value of a = '+a);
-console.log('value of b = '+b)
-// todo **=
-console.log(a**=b); //? same as a = a**b;
-console.log('value of a = '+a);
-console.log('value of b = '+b)
-// let x = 10;
-// x += 5;
-// * JavaScript Comparison Operators
-
-// * ==	equal to
-// * ===	equal value and equal type
-//* !=	not equal
-//* !==	not equal value or not equal type
-//* >	greater than
-//* <	less than
-//* >=	greater than or equal to
-//* <=	less than or equal to
-let x=5;
-// ==	equal to	x == 8	false
-// console.log(5=="5");
-// console.log(5==="5");	
-// x == 5	true	
-// x == "5"	true	
-// ===	equal value and equal type	x === 5	true	
-// x === "5"	false	
-// !=	not equal	x != 8	true !==
-// !==	not equal value or not equal type	x !== 5	false	
-// x !== "5"	true	
-// x !== 8	true	
-// >	greater than	x > 8	false	
-// <	less than	x < 8	true	
-// console.log(x>=6);
-// >=	greater than or equal to	x >= 8	false	
-// <=	less than or equal to	x <= 8	true
-
-// todo Javascript Logical operators
-
-//* && Logical and not
-//* || logical or
-//* ! logical not
-
-let a1 = 4;
-let b1 = 7;
-// And (&&) Logical operator
-if(a1>2 && b1>8 ){
-    console.log(true);
+function echo(name, num) {
+    console.log("Evaluating the " + name + " side");
+    return num;
 }
-else{
-    console.log(false)
-}
+// Notice the division operator (/)
+console.log(echo("left", 6) / echo("right", 2));
 
-if(a1>2 && b1>6 ){
-    console.log(true);
-}
-else{
-    console.log(false)
-}
-
-// Or Operator
-if(a1<2 || b1>8 ){
-    console.log(true);
-}
-else{
-    console.log(false)
-}
-
-if(a1>2 || b1>6 ){
-    console.log(true);
-}
-else{
-    console.log(false)
-}
+// let mark_mass, john_mass, john_height, mark_height;
+// mark_mass = 78;
+// mark_height = 1.69;
+// john_mass = 92;
+// john_height = 1.95;
+// let mark_bmi = mark_mass / (mark_height**2);
+// console.log ('Mark\'s BMI is ' + mark_bmi);
+// let john_bmi = john_mass / (john_height*john_height);
+// console.log ('John\'s BMI is ' + john_bmi);
 
 
+// String literals 
+const firstName = "Durgesh";
+const lastName = "Tanwar";
 
-// todo JavaScript Type Operators
-// typeof (Returns the type of a variable)
-// instanceof (Returns true if an object is an instance of an object type)
-
-// Conditional Operators (Ternary Operators)
-let age= 26;
-//  variablename = (condition) ? value1:value2 
-let voteable = (age >= 18) ? 'he can vote' : 'He can not vote';
-console.log(voteable);
-// Conditional Operator 
+console.log(`I am ${firstName} ${lastName} and my age is ${now - 1988}`);
+console.log(`Bharat humko jaan se pyara hai 
+sabse pyara gulistan hamara hai`);
