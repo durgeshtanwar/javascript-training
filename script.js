88364c12190425c0109e23fd407dbfec3cfe8c69
unknown <tanwardurgesh@gmail.com>
2021-10-18 16:32:31 +0530

    added the if else conditions

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-
-// Operator Precedence
-/* 
-the order in which operators works if there are more then one operators is called operator precedence. 
-
-Operator precedence determines how operators are parsed concerning each other.
-Operators with higher precedence become the operands of operators with lower precedence.
-
-*/
-let now = 2021;
-console.log(now - 1989 > now - 2000 );
-// why subtraction happened before the comparision?
-// This is made possible because of operator precedence
-
-console.log(3+4*5);
-// ? What will be the output of this?
-
-console.log(4 * 3 ** 2); // 4 * 9
-
-// ? what will be output of this?
-
-let a;
-let b;
-
-console.log(a = b = 5);
-// expected output: 5
-
-// In below example if the operator op2 have higher precedence then op2 will be solved first.
-// a OP1 b OP2 c
-
-console.log(3 + 10 * 2);   // logs 23
-console.log(3 + (10 * 2)); // logs 23 because parentheses here are superfluous
-console.log((3 + 10) * 2); // logs 26 because the parentheses change the order
-
-function echo(name, num) {
-    console.log("Evaluating the " + name + " side");
-    return num;
+// conditions
+// what are the conditions? 
+// example
+// program to determine if the person is eligible for the voting?
+let person = "Nandini";
+let her_age = 11;
+console.log(`Is ${person}'s age is good for the voting lets'find out`);
+console.log(`So her age is ${her_age} 🤔`);
+if(her_age >= 18){
+    console.log(`yep! ${person} can vote because her age is ${her_age} 😊`);
 }
-// Notice the division operator (/)
-console.log(echo("left", 6) / echo("right", 2));
-
-// let mark_mass, john_mass, john_height, mark_height;
-// mark_mass = 78;
-// mark_height = 1.69;
-// john_mass = 92;
-// john_height = 1.95;
-// let mark_bmi = mark_mass / (mark_height**2);
-// console.log ('Mark\'s BMI is ' + mark_bmi);
-// let john_bmi = john_mass / (john_height*john_height);
-// console.log ('John\'s BMI is ' + john_bmi);
+else {
+    console.log(`Sorry! ${person} your age is ${her_age} and you have ${18-her_age} years left for the voting. 😢`);
+}
 
 
-// String literals 
-const firstName = "Durgesh";
-const lastName = "Tanwar";
+const birthYear = 1945;
+if(birthYear<2000){
+    console.log('this person was born in 20th Century.')
+}
+else {
+    console.log(`this person was born in 21 century`);
+}
 
-console.log(`I am ${firstName} ${lastName} and my age is ${now - 1988}`);
-console.log(`Bharat humko jaan se pyara hai 
-sabse pyara gulistan hamara hai`);
+// 1 Write a Javascript program to find maximum between two numbers.
+// 2 Write a Javascript program to find maximum between three numbers.
+// 3 Write a Javascript program to check whether a number is negative, positive or zero.
+// 4 Write a Javascript program to check whether a number is divisible by 5 and 11 or not.
+// 5 Write a javascript program to check whether a number is even or odd.
+// 6 Write a javascript program to check whether a year is leap year or not.
+// 7 Write a javascript program to check whether a character is alphabet or not.
+// Write a javascript program to input any alphabet and check whether it is vowel or consonant.
+// Write a javascript program to input any character and check whether it is alphabet, digit or special character.
+// Write a Javascript program to check whether a character is uppercase or lowercase alphabet.
+// Write a Javascript program to input week number and print week day.
+// Write a Javascript program to input month number and print number of days in that month.
+// Write a Javascript program to count total number of notes in given amount.
+// Write a Javascript program to input angles of a triangle and check whether triangle is valid or not.
+// Write a Javascript program to input all sides of a triangle and check whether triangle is valid or not.
+// Write a Javascript program to check whether the triangle is equilateral, isosceles or scalene triangle.
+// Write a Javascript program to find all roots of a quadratic equation.
+// Write a Javascript program to calculate profit or loss.
+// Write a Javascript program to input marks of five subjects Physics, Chemistry, Biology, Mathematics and Computer. Calculate percentage and grade according to following:
+//      Percentage >= 90% : Grade A
+//      Percentage >= 80% : Grade B
+//      Percentage >= 70% : Grade C
+//      Percentage >= 60% : Grade D
+//      Percentage >= 40% : Grade E
+//      Percentage < 40% : Grade F
+// Write a Js program to input basic salary of an employee and calculate its Gross salary according to following:
+//      Basic Salary <= 10000 : HRA = 20%, DA = 80%
+//      Basic Salary <= 20000 : HRA = 25%, DA = 90%
+//      Basic Salary > 20000 : HRA = 30%, DA = 95%
+// Write a js program to input electricity unit charges and calculate total electricity bill according to the given condition:
+//       For first 50 units Rs. 0.50/unit
+//       For next 100 units Rs. 0.75/unit
+//       For next 100 units Rs. 1.20/unit
+//       For unit above 250 Rs. 1.50/unit
+//       An additional surcharge of 20% is added to the bill
