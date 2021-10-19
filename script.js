92249c933273433d9ffe4f1bde1c95ce61e0b0c9
unknown <tanwardurgesh@gmail.com>
2021-10-19 12:26:50 +0530

    added numbers to the problems

--- script.js
+++ script.js
@@ -9,18 +9,37 @@ console.log(`So her age is ${her_age} 🤔`);
 if(her_age >= 18){
     console.log(`yep! ${person} can vote because her age is ${her_age} 😊`);
 }
-else {
-    console.log(`Sorry! ${person} your age is ${her_age} and you have ${18-her_age} years left for the voting. 😢`);
+else if(her_age >= 12) {
+    // console.log(`Sorry! ${person} your age is ${her_age} and you have ${18-her_age} years left for the voting. 😢`);
+    console.log('she is more the 12');
+}
+else{
+    
 }
 
 
-const birthYear = 1945;
-if(birthYear<2000){
-    console.log('this person was born in 20th Century.')
-}
-else {
-    console.log(`this person was born in 21 century`);
-}
+
+
+// const birthYear = 2002;
+// if(birthYear<2000){
+//     console.log('this person was born in 20th Century.')
+// }
+// else {
+//     console.log(`this person was born in 21 century`);
+// }
+
+// let value = 10;
+// if(value%2==0){
+//     console.log(`even`);
+// }
+// else {
+//     console.log('Odd');
+// }
+// Control Structure
+
+
+
+
 
 // 1 Write a Javascript program to find maximum between two numbers.
 // 2 Write a Javascript program to find maximum between three numbers.
@@ -29,25 +48,25 @@ else {
 // 5 Write a javascript program to check whether a number is even or odd.
 // 6 Write a javascript program to check whether a year is leap year or not.
 // 7 Write a javascript program to check whether a character is alphabet or not.
-// Write a javascript program to input any alphabet and check whether it is vowel or consonant.
-// Write a javascript program to input any character and check whether it is alphabet, digit or special character.
-// Write a Javascript program to check whether a character is uppercase or lowercase alphabet.
-// Write a Javascript program to input week number and print week day.
-// Write a Javascript program to input month number and print number of days in that month.
-// Write a Javascript program to count total number of notes in given amount.
-// Write a Javascript program to input angles of a triangle and check whether triangle is valid or not.
-// Write a Javascript program to input all sides of a triangle and check whether triangle is valid or not.
-// Write a Javascript program to check whether the triangle is equilateral, isosceles or scalene triangle.
-// Write a Javascript program to find all roots of a quadratic equation.
-// Write a Javascript program to calculate profit or loss.
-// Write a Javascript program to input marks of five subjects Physics, Chemistry, Biology, Mathematics and Computer. Calculate percentage and grade according to following:
+// 8 Write a javascript program to input any alphabet and check whether it is vowel or consonant.
+// 9 Write a javascript program to input any character and check whether it is alphabet, digit or special character.
+// 10 Write a Javascript program to check whether a character is uppercase or lowercase alphabet.
+// 11 Write a Javascript program to input week number and print week day.
+// 12 Write a Javascript program to input month number and print number of days in that month.
+// 14 Write a Javascript program to count total number of notes in given amount.
+// 15 Write a Javascript program to input angles of a triangle and check whether triangle is valid or not.
+// 16 Write a Javascript program to input all sides of a triangle and check whether triangle is valid or not.
+// 17 Write a Javascript program to check whether the triangle is equilateral, isosceles or scalene triangle.
+// 18 Write a Javascript program to find all roots of a quadratic equation.
+// 19 Write a Javascript program to calculate profit or loss.
+// 20 Write a Javascript program to input marks of five subjects Physics, Chemistry, Biology, Mathematics and Computer. Calculate percentage and grade according to following:
 //      Percentage >= 90% : Grade A
 //      Percentage >= 80% : Grade B
 //      Percentage >= 70% : Grade C
 //      Percentage >= 60% : Grade D
 //      Percentage >= 40% : Grade E
 //      Percentage < 40% : Grade F
-// Write a Js program to input basic salary of an employee and calculate its Gross salary according to following:
+// 21 Write a Js program to input basic salary of an employee and calculate its Gross salary according to following:
 //      Basic Salary <= 10000 : HRA = 20%, DA = 80%
 //      Basic Salary <= 20000 : HRA = 25%, DA = 90%
 //      Basic Salary > 20000 : HRA = 30%, DA = 95%
